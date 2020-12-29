91529724746fe7e3b8edbb5f8dc62b9c7f16f731
exodusht <1847944340@qq.com>
2020-12-29 16:32:09 +0800

    fix:Admin表继承多用户信息接口

--- Entity/Admin.php
+++ Entity/Admin.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Repository\AdminRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -9,7 +10,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @ORM\Entity(repositoryClass=AdminRepository::class)
  * @ORM\Table(name="admin", options={"comment":"管理员表"})
  */
-class Admin
+class Admin implements UserInterface
 {
     /**
      * @var string
